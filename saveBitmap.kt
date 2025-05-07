e69c3ab50a21caa6d5e149155e5ff586fa1218c0
skwad3d <47848301+skwad3d@users.noreply.github.com>
2025-05-07 20:18:21 +0200

    Create saveBitmap.kt

---
+++ saveBitmap.kt
@@ -0,0 +1,23 @@
+package com.example.heightmapapp
+
+import android.graphics.Bitmap
+import android.os.Environment
+import android.widget.Toast
+import java.io.File
+import java.io.FileOutputStream
+
+fun saveBitmap(bitmap: Bitmap) {
+    val filename = "heightmap_${System.currentTimeMillis()}.png"
+    val dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+    val file = File(dir, filename)
+
+    FileOutputStream(file).use {
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, it)
+    }
+
+    Toast.makeText(
+        null,
+        "Saved to ${file.absolutePath}",
+        Toast.LENGTH_LONG
+    ).show()
+}
